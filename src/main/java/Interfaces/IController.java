92eea664d2597e2a64d6d9ad58a09ab9afa97fa0
Gilarte <rafagilarte@gmail.com>
2022-04-20 14:52:05 +0200

    interfaces subidas

---
+++ src/main/java/Interfaces/IController.java
@@ -0,0 +1,8 @@
+package Interfaces;
+
+public interface IController {
+
+	public void startGame();
+	public void showCity();
+	public void opcMenu();
+}
---
+++ src/main/java/Interfaces/IUsuarioDao.java
@@ -0,0 +1,10 @@
+package Interfaces;
+
+public interface IUsuarioDao {
+
+	
+	public boolean addUser();
+	public void showUsers();
+	public boolean deleteUser();
+	
+}
